919bd7113abda55857c60f67b9e4c56075e94af1
Alard <alard@example.com>
2013-01-25 05:20:52 -0500

    Improved project.json loading.

--- set-projects-json.rb
+++ set-projects-json.rb
@@ -1,6 +1,19 @@
 require "rubygems"
 require "redis"
+require "json"
+require "./geoip"
 
 $redis = Redis.new(:db=>1)
-$redis.set("warriorhq:projects_json", File.read(ARGV[0]))
+
+data = JSON.parse(File.read(ARGV[0]).gsub(/^\/\/.+/, ""))
+data["projects"].each do |project|
+  if project["host"] and not project["lat_lng"]
+    geo = GeoIP.new[project["host"]]
+    if geo and geo["latitude"] and geo["latitude"]=~/[-.0-9]+/
+      project["lat_lng"] = [ geo["latitude"].to_i, geo["longitude"].to_i ]
+    end
+  end
+end
+
+$redis.set("warriorhq:projects_json", JSON.dump(data))
 
